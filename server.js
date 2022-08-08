9afa46ee4506725ec2bd750fa917cd7d650126fe
jeaffbozos <jeaffbozos@gmail.com>
2022-08-08 12:09:45 -0700

    Change PORT

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/postData', async (req, res) => {
     
   });
 
-
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log('Server is running on port 5000')
 })
